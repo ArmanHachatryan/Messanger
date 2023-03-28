1f7370a72feefef2d9a78138f4816fe3e4e3a407
Арман <58301912+ArmanHachatryan@users.noreply.github.com>
2023-03-28 16:30:11 +0300

    Fetch

--- Backend/Backend/Controllers/HomeController.cs
+++ Backend/Backend/Controllers/HomeController.cs
@@ -4,12 +4,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Backend.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class HomeController : ControllerBase
+    public class HomeController : Controller
     {
         private readonly IPostgreDbContext _context;
 
@@ -21,21 +23,37 @@ namespace Backend.Controllers
         [HttpGet]
         public IActionResult GetData()
         {
+            //AppData model = new AppData()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Text = "Test",
+            //    Status = 1,
+            //    PhoneNumber = "+77777777",
+            //    DateTime = DateTime.Now,
+            //    Sender = "Alex"
 
-            AppData model = new AppData()
-            {
-                Id = Guid.NewGuid(),
-                Text = "Test",
-                Status = 1,
-                PhoneNumber = "+77777777",
-                DateTime = DateTime.Now,
-                Sender = "Alex"
-
-            };
-            _context.Data.Add(model);
-            _context.Save();
-            return Ok("Good");
+            //};
+            return Json(_context.Data.ToList());
         }
+
+        [HttpPost]
+        public IActionResult PostData() 
+        {
+            //AppData model = new AppData()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Text = "Test",
+            //    Status = 1,
+            //    PhoneNumber = "+77777777",
+            //    DateTime = DateTime.Now,
+            //    Sender = "Alex"
+
+            //};
+            //_context.Data.Add(model);
+            //_context.Save();
+            return Ok("good");
+        }
+        
 
     }
 }
--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+interface MessageData{
+  id: string,
+  text: string,
+  phoneNumber: number,
+  dateTime: string,
+  status: number
+
+}
 
 export default function App() {
+  const [data, setData] = useState<MessageData[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+        const response = await axios.get("https://localhost:44352/api/Home");
+
+        setData(response.data);
+    };
+
+    fetchPosts();
+  }, []);
+
+
   return (
-    <div>App</div>
+    <>
+      <h2>History</h2>
+      {data.map((post)=> {
+        const {id, text, phoneNumber, dateTime, status} = post;
+
+        return (
+          <article key={id}>
+            <p>{dateTime}, {text}, {phoneNumber}, {status}</p>
+          </article>
+        )
+      })}
+  </>
   )
+
 }
